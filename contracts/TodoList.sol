2cd9b40dcfbe32aa885fc3ed863a568f7c421eaf
animesh.kumar <animesh.kumar@rubrik.com>
2021-10-04 20:40:45 +0530

    2. Able to show task on UI

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,28 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
 	uint public taskCount = 0;
+
+	constructor() public {
+		createTask("This is the first first task");
+	}
+
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping (uint => Task) public tasks;
+
+	event TaskCreated(
+	    uint id,
+	    string content,
+	    bool completed
+  	);
+
+	function createTask(string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+		emit TaskCreated(taskCount, _content, false);
+	}
 }
